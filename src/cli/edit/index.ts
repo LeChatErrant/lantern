fc752e4451d9d78699ecd048f348dd7cf0780d84
LeChatErrant <guillaume.hector@epitech.eu>
2022-01-20 00:33:46 +0100

    revert: removing debugs in edit command

--- src/cli/edit/index.ts
+++ src/cli/edit/index.ts
@@ -1,11 +1,5 @@
-import { createDir, editFile, removeFile } from '../../utils/files';
-
 async function edit() {
-  createDir('./test/zob');
-  editFile('./test/megusta', 'nani');
-  createDir('./test/zebi');
-  removeFile('./test/misking');
-  createDir('./test/dab');
+  console.log('Coming soon !');
 }
 
 export default edit;
