f75dd486ecce861a102dd69ec6ffc09232ed2be5
LeChatErrant <guillaume.hector@epitech.eu>
2022-02-01 17:02:16 +0100

    feat: correctly handling download errors

--- src/utils/download.ts
+++ src/utils/download.ts
@@ -1,8 +1,9 @@
 import clui from 'clui';
 import download from 'download';
+import { blue } from './colors';
 
 export async function downloadWithSpinner(url: string) {
-  const spinner = new clui.Spinner(`Downloading ${url}...`);
+  const spinner = new clui.Spinner(`Downloading ${blue(url)}...`);
 
   spinner.start();
   try {
